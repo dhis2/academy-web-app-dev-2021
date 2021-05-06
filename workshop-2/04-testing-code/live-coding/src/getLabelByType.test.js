905e1dbaf9f5196535ac57c00be98897f9fdb638
Jan-Gerke Salomon <jgs.salomon@gmail.com>
2021-05-06 10:11:27 +0200

    chore(live coding): add failing tests

--- workshop-2/04-testing-code/live-coding/src/getLabelByType.test.js
+++ workshop-2/04-testing-code/live-coding/src/getLabelByType.test.js
@@ -5,3 +5,9 @@ import { getLabelByType } from '../getLabelByType'
 //   * Make sure the function returns "Top label" when the type is "mid"
 //   * Make sure the function returns "Value label" when the type is "low"
 //   * Make sure the function returns the type itself for any other type
+
+describe('DataElement', () => {
+  it('should fail', () => {
+    expect(true).toBe(false)
+  })
+})
